2ec061cda8f58ef85edad17b7a1ec89b0c80be5a
Chania Dwintania <chania.dwintania02@gmail.com>
2023-04-16 04:18:35 +0700

    Menambahkan file Test_case.py

---
+++ Test_Case.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import selfcashier1 as k
+transc = k.Transaction()
+
+
+# In[2]:
+
+
+transc.add_item()
+
+
+# In[3]:
+
+
+transc.update_item_name()
+
+
+# In[4]:
+
+
+transc.update_item_price()
+
+
+# In[5]:
+
+
+transc.update_item_qty()
+
+
+# In[6]:
+
+
+transc.delete_item()
+
+
+# In[7]:
+
+
+transc.reset_transaction()
+
+
+# In[8]:
+
+
+transc.add_item()
+
+
+# In[9]:
+
+
+transc.check_order()
+
+
+# In[10]:
+
+
+transc.total_price()
+
+
+# In[ ]:
+
+
+
+
